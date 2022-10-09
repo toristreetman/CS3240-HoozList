5b4b9e601bb03b541d62cde63affb105c873e54f
Yuxi Yang <67807877+YuxiYang0@users.noreply.github.com>
2022-10-09 17:33:24 -0400

    @9

--- app/tests.py
+++ app/tests.py
@@ -5,7 +5,4 @@ class YourTestClass(TestCase):
         def test_view_url_exists_at_desired_location(self):
                 response = self.client.get('')
                 self.assertEqual(response.status_code, 200)
-<<<<<<< HEAD
 
-=======
->>>>>>> 8efc3d683666ae49413fcc134253020baf9e7505

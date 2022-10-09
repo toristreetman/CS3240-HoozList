95cffa5ecb92aa83a57c6f56d762ae757dd1018c
Yuxi Yang <67807877+htbacssd@users.noreply.github.com>
2022-10-09 16:44:24 -0400

    @5

--- app/tests.py
+++ app/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
-def test_view_url_exists_at_desired_location(self):
-        response = self.client.get('')
-        self.assertEqual(response.status_code, 200)
+class YourTestClass(TestCase):
+        def test_view_url_exists_at_desired_location(self):
+                response = self.client.get('')
+                self.assertEqual(response.status_code, 200)
+

c2b488d4a15da5322b719db07bdc1b701c431322
KATI219 <78378999+KATI219@users.noreply.github.com>
2022-10-10 18:27:10 -0400

    Update settings.py

--- louslist/settings.py
+++ louslist/settings.py
@@ -45,6 +45,7 @@ ALLOWED_HOSTS = ['127.0.0.1',
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

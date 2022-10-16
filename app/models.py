d1b1167e313599339d3a233d0ca08ad8c3673d80
Neil Phan <nnp3axx@virginia.edu>
2022-10-16 14:31:43 -0400

    Added department and course models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+
+class Department(models.Model):
+    """
+    Model that describes a whole department.
+    """
+    subject = models.CharField(max_length=10)
+    
+    def __str__(self):
+        return self.subject
+    
+class Course(Department):
+    """
+    Model that visualizes a specific course in a department.
+    This model inherits from the Department model.
+    """
+    # Instructor Logistics
+    instructor_name = models.CharField(max_length=120)
+    instructor_email = models.CharField(max_length=60)
+    
+    # Course Logistics
+    semester_code = models.IntegerField()
+    course_num = models.IntegerField()
+    course_name = models.CharField(max_length=150)
+    section = models.CharField(max_length=5)
+    capacity = models.IntegerField()
+    location = models.CharField(max_length=70)
+    
+    # Course Timing Logistics
+    meeting_days = models.CharField(max_length=20)
+    start_time = models.CharField(max_length=30)
+    end_time = models.CharField(max_length=30)
+    
+    def __str__(self):
+        # EX: CS1110 -- 001: Introduction to Programming
+        return self.subject + self.course_num + " -- " + self.section + ": " + self.course_name
+    
+    
+    
+    
+    
+    
+    
+    
--- app/views.py
+++ app/views.py
@@ -1,10 +1,43 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from app.models import Department, Course
+import requests
+import pandas as pd
+
 # Create your views here.
 #@csrf_exempt
 def index(request):
     return HttpResponse("Welcome to the better Lou's List!")
+
 #@csrf_exempt
 def login(request):
-    return render(request, 'app.html', {})
+
+    d = Department.objects.all().order_by('subject')
+    all_departments = {
+        "department": d
+    }
+    
+    # Checks if the database has values in it.
+    # If not, update the database.
+    if not d:
+        all_departments = {}
+        url = "http://luthers-list.herokuapp.com/api/deptlist"
+        response = requests.get(url)
+        data = response.json()   
+        subjects = data
+        for s in subjects:
+            department = Department(subject = s['subject'])
+            department.save()
+        d = Department.objects.all().order_by('subject')
+        all_departments = {
+            "department": d
+        }
+            
+    return render(request, 'app.html', all_departments)
+
+
+def department(request):
+    return render(request, 'class.html')
+        
+        

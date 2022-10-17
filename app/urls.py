8ae12ace004afc0b32572f93d5940ca9ee155050
KATI219 <78378999+KATI219@users.noreply.github.com>
2022-10-17 19:20:31 -0400

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     # path('', views.index, name='index'),
     path('', views.login, name='login'),
-    path('<slug:slug>/', views.DepartmentView.as_view(), name='department')
+    #path('<slug:slug>/', views.DepartmentView.as_view(), name='department')
 ]
     
     

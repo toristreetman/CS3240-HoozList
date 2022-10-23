3a861e24dd79a4550138ce16c25d252df79f4d30
Yuxi Yang <67807877+htbacssd@users.noreply.github.com>
2022-10-23 03:01:13 -0400

    add str

--- app/tests.py
+++ app/tests.py
@@ -1,11 +1,19 @@
 from django.test import TestCase
+from django.test import Client
+import unittest
+from app.models import Department, Course, CourseDetail
 
 # Create your tests here.
 class YourTestClass(TestCase):
-        def test_view_url_exists_at_desired_location(self):
+        # def test_view_url_exists_at_desired_location(self):
+        #         response = self.client.get('')
+        #         self.assertEqual(response.status_code, 200)
+        def test_view_url_exists_at_second_location(self):
                 response = self.client.get('')
                 self.assertEqual(response.status_code, 200)
-        def test_view_url_exists_at_second_location(self):
-                response = self.client.get('dept-list/')
-                self.assertEqual(response.status_code, 200)
+
+class TestModels(TestCase):
+        def DepartmentStr(self):
+                dept = Department.objects.create(slug="PHIL")
+                self.assertEqual(str(dept),"PHIL")
 

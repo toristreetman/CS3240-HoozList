b18d2082cffe1b8a9beab009ccccc1cb248e0702
Yuxi Yang <67807877+htbacssd@users.noreply.github.com>
2022-10-23 17:35:37 -0400

    add tests

--- app/tests.py
+++ app/tests.py
@@ -3,8 +3,17 @@ from django.test import Client
 import unittest
 from app.models import Department, Course, CourseDetail
 
+from django.db import models
+
+
+# from django.test import LiveServerTestCase
+# from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+
 # Create your tests here.
-#check if the main page could be found
+#check if the pages could be found
+# driver = webdriver.Chrome('/Users/yuxismac/chromedriver')
+
 class YourTestClass(TestCase):
         def test_view_url(self):
                 response = self.client.get('')
@@ -13,7 +22,7 @@ class YourTestClass(TestCase):
                 response1 = self.client.get('/dept-list/')
                 self.assertEqual(response1.status_code, 200)
 
-#check if department model's to string method is working correctly
+# check if the two models that are essential to the first feature is working correctly
 
         def departmentStr(self):
                 dept = Department.objects.create(slug="PHIL")
@@ -23,12 +32,21 @@ class YourTestClass(TestCase):
                 co = Course.objects.create(subject="CS",course_num="1110",section = "001",
                 course_name="Introduction to Programming")
                 self.assertEqual(str(co),"CS1110 -- 001: Introduction to Programming")
+#Check if the content in the log-in page is working correctly
         def test_view_log(self):
                 response = self.client.get('')
                 self.assertContains(response, "Log In")
         def test_view_hooz(self):
                 response = self.client.get('')
                 self.assertContains(response, "HoozList")
+#Check if dept-list website disallows user to view courses without being logged in
+        def test_view_notloggedin(self):
+                response = self.client.get('/dept-list/')
+                self.assertContains(response, "Please sign in to access the course catalog!")
         
+
+
+
+
        
 

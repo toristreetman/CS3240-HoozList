8c25495de584e59ed7190f99e9a2e20b03b9ff52
Yuxi Yang <67807877+htbacssd@users.noreply.github.com>
2022-10-23 01:36:32 -0400

    add setup

--- app/tests.py
+++ app/tests.py
@@ -2,6 +2,13 @@ from django.test import TestCase
 
 # Create your tests here.
 class YourTestClass(TestCase):
+        def setUpTestData(cls):
+                print("setUpTestData: Run once to set up non-modified data for all class methods.")
+                pass
+
+        def setUp(self):
+                print("setUp: Run once for every test method to setup clean data.")
+                pass
         def test_view_url_exists_at_desired_location(self):
                 response = self.client.get('')
                 self.assertEqual(response.status_code, 200)

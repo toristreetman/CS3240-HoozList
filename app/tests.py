cb0e07b77fe196dfc74ae255bfa67a76fb34318f
Yuxi Yang <67807877+htbacssd@users.noreply.github.com>
2022-10-23 02:31:36 -0400

    second test

--- app/tests.py
+++ app/tests.py
@@ -5,4 +5,7 @@ class YourTestClass(TestCase):
         def test_view_url_exists_at_desired_location(self):
                 response = self.client.get('')
                 self.assertEqual(response.status_code, 200)
+        def test_view_url_exists_at_second_location(self):
+                response = self.client.get('dept-list/')
+                self.assertEqual(response.status_code, 200)
 

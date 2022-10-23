888813e9653771da71dfb511c540d4753639cf2d
Yuxi Yang <67807877+htbacssd@users.noreply.github.com>
2022-10-23 11:13:10 -0400

    edit name

--- app/tests.py
+++ app/tests.py
@@ -6,7 +6,7 @@ from app.models import Department, Course, CourseDetail
 # Create your tests here.
 #check if the main page could be found
 class YourTestClass(TestCase):
-        def test_view_url_exists_at_desired_location(self):
+        def test_view_url(self):
                 response = self.client.get('')
                 self.assertEqual(response.status_code, 200)
         # def test_view_url_exists_at_second_location(self):
@@ -14,12 +14,12 @@ class YourTestClass(TestCase):
         #         self.assertEqual(response.status_code, 200)
 
 #check if department model's to string method is working correctly
-class TestModels(TestCase):
-        def DepartmentStr(self):
+
+        def departmentStr(self):
                 dept = Department.objects.create(slug="PHIL")
                 self.assertEqual(str(dept),"PHIL")
 
-        def Course(self):
+        def courseStr(self):
                 co = Course.objects.create(subject="CS",course_num="1110",section = "001",course_name="Introduction to Programming")
                 self.assertEqual(str(co),"CS1110 -- 001: Introduction to Programming")
 

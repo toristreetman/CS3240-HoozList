3108f63d73b5d18e9d455c3b6d4d4cff7d3dbc33
Yuxi Yang <67807877+htbacssd@users.noreply.github.com>
2022-10-23 01:31:01 -0400

    remove

--- app/tests.py
+++ app/tests.py
@@ -2,11 +2,6 @@ from django.test import TestCase
 
 # Create your tests here.
 class YourTestClass(TestCase):
-        def setUp(self):
-                #Setup run before every test method
-                pass
-        def tearDown(self):
-                #Clean up run after every test method
         def test_view_url_exists_at_desired_location(self):
                 response = self.client.get('')
                 self.assertEqual(response.status_code, 200)

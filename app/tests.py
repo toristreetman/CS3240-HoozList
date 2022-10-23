2d8bc111851eaf55b4a536a7538ef32ab6218b35
Yuxi Yang <67807877+htbacssd@users.noreply.github.com>
2022-10-23 01:27:57 -0400

    removed block

--- app/tests.py
+++ app/tests.py
@@ -7,9 +7,6 @@ class YourTestClass(TestCase):
                 pass
         def tearDown(self):
                 #Clean up run after every test method
-        
-
-
         def test_view_url_exists_at_desired_location(self):
                 response = self.client.get('')
                 self.assertEqual(response.status_code, 200)

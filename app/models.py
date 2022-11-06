23d5d9bcadda2466b07d9c3cd691b16c52b03e6d
KATI219 <78378999+KATI219@users.noreply.github.com>
2022-11-06 18:57:02 -0500

    userprofile for anonymous user

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.views.generic.detail import DetailView
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AnonymousUser
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 
@@ -69,6 +69,10 @@ class UserProfile(models.Model):
 # this receiver detects whenever a user is created with the default django User model
 # and creates a UserProfile associated with that user
 @receiver(post_save, sender = User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+@receiver(post_save, sender = AnonymousUser)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)

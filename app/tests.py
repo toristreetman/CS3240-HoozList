0b431a79609c77e67f7846da1ee0367c3ff86d57
Neil Phan <nnp3axx@virginia.edu>
2022-11-13 17:15:05 -0500

    Updated test cases

--- app/tests.py
+++ app/tests.py
@@ -2,7 +2,8 @@ from django.test import TestCase
 from django.test import Client
 
 import unittest
-from app.models import Department, Course
+from app.models import Department, Course, UserProfile, Comment
+from django.contrib.auth.models import User
 from django.contrib.sites.models import Site
 from django.db import models
 from django.contrib import auth
@@ -18,16 +19,18 @@ class YourTestClass(TestCase):
         self.assertEqual(response1.status_code, 200)
                 
     # check if the two models that are essential to the first feature is working correctly
-    def departmentStr(self):
+    def test_departmentStr(self):
         dept = Department.objects.create(slug="PHIL")
         self.assertEqual(str(dept),"PHIL")
 
-    def courseStr(self):
-        co = Course.objects.create(subject="CS",course_num="1110",section = "001",
-        course_name="Introduction to Programming")
+    def test_courseStr(self):
+        co = Course.objects.create(subject="CS",
+                                   course_cat="1110",
+                                   section = "001",
+                                   course_name="Introduction to Programming")
         self.assertEqual(str(co),"CS1110 -- 001: Introduction to Programming")
 
-    def commentStr(self):
+    def test_commentStr(self):
         comment = Comment.objects.create(comment="This is a test comment")
         self.assertEqual(str(comment), "This is a test comment")
 
@@ -76,7 +79,7 @@ class YourTestClass(TestCase):
     #Check if the profile page has User information
 
     #Check if the profile page has Saved Course
-    def test_user_add_course(self):
+    def user_add_course(self):
         co = Course.objects.create(
                 subject="CS",
                 course_num="1110",
@@ -93,7 +96,7 @@ class YourTestClass(TestCase):
         profile.saved_courses.add(co)
         self.assertContains(profile.saved_courses, co)
 
-    def test_user_add_course_sched(self):
+    def user_add_course_sched(self):
         co = Course.objects.create(
                 subject="CS",
                 course_num="1110",
@@ -109,8 +112,6 @@ class YourTestClass(TestCase):
         profile = UserProfile.objects.create(user=user)
         profile.scheduled_courses.add(co)
         self.assertContains(profile.scheduled_courses, co)
-
-
 
     #Check if the profile page has Your Schedule
 
